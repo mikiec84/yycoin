3e80609562878ce4960cb6ece206b2baaa835767
smartman <smartman2014@qq.com>
2015-08-17 15:53:28 +0800

    update SQL

--- doc/script.sql
+++ doc/script.sql
@@ -1,4 +1,10 @@
 alter table t_center_out add column remoteAllocate int(11) DEFAULT 0
+--2015/6/6 产品配置增加激励字段
+alter table T_CENTER_ZJRCPRODUCT add column motivationMoney double DEFAULT '0'
+alter table T_CENTER_ZJRCBASE add column motivationMoney double DEFAULT '0'
 
 --2015/8/15 新产品申请去掉产品管理部审批环节
 delete from t_center_oamenuitem where menuitemname ='新产品-产品管理部审批' and id='1021'
+
+--2015/8/16 批量更新中信产品配置
+insert into t_center_oamenuitem values(9035,'批量更新产品配置','../sailImport/batchUpdateZJRCProduct.jsp',90,1,9031,99,'批量更新产品配置')
