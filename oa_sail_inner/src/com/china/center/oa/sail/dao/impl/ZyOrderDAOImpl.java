06b0b8d76d5c26daad93b68d739836bf1c0e3969
kingofhawks <kingofhawks@qq.com>
2016-04-20 21:24:48 +0800

    update #222

---
+++ oa_sail_inner/src/com/china/center/oa/sail/dao/impl/ZyOrderDAOImpl.java
@@ -0,0 +1,11 @@
+package com.china.center.oa.sail.dao.impl;
+
+import com.china.center.jdbc.inter.impl.BaseDAO;
+import com.china.center.oa.sail.bean.ZyOrderBean;
+import com.china.center.oa.sail.dao.ZyOrderDAO;
+
+
+public class ZyOrderDAOImpl extends BaseDAO<ZyOrderBean, ZyOrderBean> implements ZyOrderDAO
+{
+
+}
