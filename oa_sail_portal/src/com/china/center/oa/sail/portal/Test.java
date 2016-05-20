6308dbcd61b466fd1b04541105d8ff54e0d9ee30
smartman <smartman2014@qq.com>
2016-05-20 16:56:34 +0800

    update #240

--- oa_sail_portal/src/com/china/center/oa/sail/portal/Test.java
+++ oa_sail_portal/src/com/china/center/oa/sail/portal/Test.java
@@ -11,11 +11,25 @@ import com.china.center.tools.CommonTools;
 import com.china.center.tools.MathTools;
 import com.china.center.tools.StringTools;
 import com.china.center.tools.TimeTools;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -25,8 +39,59 @@ import java.util.*;
  * To change this template use File | Settings | File Templates.
  */
 public class Test {
+    private void generateQRCode_general(String myCodeText)throws WriterException {
+        String filePath = "D:/CrunchifyQR.png";
+        int size = 250;
+        String fileType = "png";
+        File myFile = new File(filePath);
+        try {
+
+            Map<EncodeHintType, Object> hintMap = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+            hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+            // Now with zxing version 3.2.1 you could change border size (white border size to just 1)
+            hintMap.put(EncodeHintType.MARGIN, 1); /* default = 4 */
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
+//            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+//            BitMatrix byteMatrix = qrCodeWriter.encode(myCodeText, BarcodeFormat.QR_CODE, size,
+//                    size, hintMap);
+
+            com.google.zxing. MultiFormatWriter writer =new MultiFormatWriter();
+            BitMatrix byteMatrix = writer.encode(myCodeText, BarcodeFormat.CODE_128,150, 150);
+
+            int CrunchifyWidth = byteMatrix.getWidth();
+            BufferedImage image = new BufferedImage(CrunchifyWidth, CrunchifyWidth,
+                    BufferedImage.TYPE_INT_RGB);
+            image.createGraphics();
+
+            Graphics2D graphics = (Graphics2D) image.getGraphics();
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, CrunchifyWidth, CrunchifyWidth);
+            graphics.setColor(Color.BLACK);
+
+            for (int i = 0; i < CrunchifyWidth; i++) {
+                for (int j = 0; j < CrunchifyWidth; j++) {
+                    if (byteMatrix.get(i, j)) {
+                        graphics.fillRect(i, j, 1, 1);
+                    }
+                }
+            }
+            ImageIO.write(image, fileType, myFile);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\n\nYou have successfully created QR Code.");
+
+    }
 
     public String getInCondition(String value){
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String specifiedDay = sdf.format(date);
+        System.out.println(specifiedDay);
         //SO1602230844017663321;SO1602231628017664378;
         StringBuilder sb = new StringBuilder();
         String[] values = value.split(";");
@@ -103,6 +168,7 @@ public class Test {
         sdf1.parse(date);
 
         Test t = new Test();
+        t.generateQRCode_general("CK111222");
         System.out.println(t.getInCondition("SO1602230844017663321;SO1602231628017664378;"));
         System.out.println(t.getInCondition("SO1603311420017767434;"));
         String name = null;
